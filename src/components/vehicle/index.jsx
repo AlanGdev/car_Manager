e2787936418a310a87ec787ca1b6a6cc6f404ede
AlanGdev <alan.grolleau@gmail.com>
2025-03-26 10:24:52 +0100

    Deboggage et Alert en cas de véhicule non enregistré

--- src/components/vehicle/index.jsx
+++ src/components/vehicle/index.jsx
@@ -1,10 +1,12 @@
+import { Alert } from 'react-bootstrap'
 function Vehicle() {
   const vehicleData = JSON.parse(localStorage.getItem('vehicleData'))
 
   console.log(vehicleData)
   return (
     <>
-      <h2>{`${vehicleData.marque} ${vehicleData.model}`}</h2>
+      {!vehicleData && <Alert variant="danger">Aucun véhicule enregistré...</Alert>}
+      {vehicleData && <h2>{`${vehicleData.marque} ${vehicleData.model}`}</h2>}
     </>
   )
 }
