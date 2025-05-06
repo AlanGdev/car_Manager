fb147c8e28f50ccbae9ac68b903e55186f09f6af
Alan.Grolleau <alan.grolleau@gmail.com>
2025-05-06 13:55:46 +0200

    rajout d'un * dans les routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/vehicule" element={<VehiculePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
           <Footer />
         </div>
